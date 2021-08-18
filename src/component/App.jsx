5349821c1287bf7d38e6c940a01699395d39cb04
Himali Malvawala <himalimalvawala@gmail.com>
2021-08-18 15:09:17 +0530

    Create Notes through map

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Footer  from "./Footer";
 import Note from "./Note.jsx";
+import notes from "../notes";
 
 function App(){
     return (<div>
         <Header />
-        <Note />
-        <Footer />
+        {notes.map((createEntry) => (
+            <Note
+                key= {createEntry.key}
+                title={createEntry.title}
+                content={createEntry.content}
+            />
+        ))}
+        <Footer /> 
     </div>);
 }
 
